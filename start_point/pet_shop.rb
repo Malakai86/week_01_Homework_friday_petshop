378398af7daa279248dffb795078537806955d11
mike <>
2020-02-28 17:05:53 +0000

    Forth test passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -7,21 +7,7 @@ end
 def total_cash(shop)
   return shop[:admin][:total_cash]
 end
-
+# function works for test 3 and 4?
 def add_or_remove_cash(shop, cash)
     shop[:admin][:total_cash] += cash
 end
-
-# def lend_money(lender, lendee, amount)
-#   lender[:monies] -= amount
-#   lendee[:monies] += amount
-# end
-# def test_lend_money
-#   # @person 2 has £2, @person1 has 1
-#   # @person2 lends £2 to @person1
-#   lend_money(@person2, @person1, 2)
-#   # assert that @person2 has 0 left
-#   assert_equal(0, @person2[:monies])
-#   # assert that @person1 now has £3
-#   assert_equal(3, @person1[:monies])
-# end
